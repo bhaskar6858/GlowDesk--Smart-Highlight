99c0b5de5070cbe374b2f080d0114c3d63318df2
bhaskarsingh <bhaskarsingh@BHASKARSING914B>
2025-04-27 12:23:48 +0530

    Final Project Updated

--- GlowDesk/Form1.Designer.cs
+++ GlowDesk/Form1.Designer.cs
@@ -28,64 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             recentListBox = new ListBox();
-            titleLabel = new Label();
-            scanButton = new Button();
-            autoScanCheckbox = new CheckBox();
+            label1 = new Label();
+            buttonScanNow = new Button();
+            checkBoxAutoScan = new CheckBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            numericThreshold = new NumericUpDown();
+            label3 = new Label();
+            fileWatcher = new FileSystemWatcher();
+            buttonBrowse = new Button();
+            contextMenuStrip = new ContextMenuStrip(components);
+            copyPathMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)numericThreshold).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fileWatcher).BeginInit();
+            contextMenuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // recentListBox
             // 
+            recentListBox.ContextMenuStrip = contextMenuStrip;
+            recentListBox.DrawMode = DrawMode.OwnerDrawFixed;
             recentListBox.FormattingEnabled = true;
-            recentListBox.Location = new Point(129, 146);
+            recentListBox.Location = new Point(46, 129);
             recentListBox.Name = "recentListBox";
-            recentListBox.Size = new Size(571, 260);
+            recentListBox.Size = new Size(1035, 388);
             recentListBox.TabIndex = 0;
-            recentListBox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            recentListBox.DrawItem += recentListBox_DrawItem;
+            recentListBox.SelectedIndexChanged += recentListBox_SelectedIndexChanged;
+            recentListBox.DoubleClick += recentListBox_DoubleClick;
+            recentListBox.MouseDown += recentListBox_MouseDown;
             // 
-            // titleLabel
+            // label1
             // 
-            titleLabel.AutoSize = true;
-            titleLabel.Font = new Font("Segoe UI", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            titleLabel.ForeColor = SystemColors.ControlText;
-            titleLabel.Location = new Point(119, 46);
-            titleLabel.Name = "titleLabel";
-            titleLabel.Size = new Size(617, 50);
-            titleLabel.TabIndex = 1;
-            titleLabel.Text = "GlowDesk- Recent Activity Tracker";
-            titleLabel.Click += label1_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(34, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(617, 50);
+            label1.TabIndex = 1;
+            label1.Text = "GlowDesk- Recent Activity Tracker";
+            label1.Click += label1_Click;
             // 
-            // scanButton
+            // buttonScanNow
             // 
-            scanButton.Location = new Point(129, 514);
-            scanButton.Name = "scanButton";
-            scanButton.Size = new Size(150, 46);
-            scanButton.TabIndex = 2;
-            scanButton.Text = "Scan Now";
-            scanButton.UseVisualStyleBackColor = true;
-            scanButton.Click += button1_Click;
+            buttonScanNow.Location = new Point(579, 600);
+            buttonScanNow.Name = "buttonScanNow";
+            buttonScanNow.Size = new Size(150, 46);
+            buttonScanNow.TabIndex = 2;
+            buttonScanNow.Text = "Scan Now";
+            buttonScanNow.UseVisualStyleBackColor = true;
+            buttonScanNow.Click += buttonScanNow_Click;
             // 
-            // autoScanCheckbox
+            // checkBoxAutoScan
             // 
-            autoScanCheckbox.AutoSize = true;
-            autoScanCheckbox.Location = new Point(393, 515);
-            autoScanCheckbox.Name = "autoScanCheckbox";
-            autoScanCheckbox.Size = new Size(315, 36);
-            autoScanCheckbox.TabIndex = 3;
-            autoScanCheckbox.Text = "Auto Scan in Background";
-            autoScanCheckbox.UseVisualStyleBackColor = true;
+            checkBoxAutoScan.AutoSize = true;
+            checkBoxAutoScan.Location = new Point(778, 606);
+            checkBoxAutoScan.Name = "checkBoxAutoScan";
+            checkBoxAutoScan.Size = new Size(315, 36);
+            checkBoxAutoScan.TabIndex = 3;
+            checkBoxAutoScan.Text = "Auto Scan in Background";
+            checkBoxAutoScan.UseVisualStyleBackColor = true;
+            checkBoxAutoScan.CheckedChanged += checkBoxAutoScan_CheckedChanged;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 602);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Show Top";
+            // 
+            // numericThreshold
+            // 
+            numericThreshold.Location = new Point(170, 600);
+            numericThreshold.Maximum = new decimal(new int[] { 30, 0, 0, 0 });
+            numericThreshold.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericThreshold.Name = "numericThreshold";
+            numericThreshold.Size = new Size(84, 39);
+            numericThreshold.TabIndex = 5;
+            numericThreshold.Value = new decimal(new int[] { 6, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(260, 602);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 32);
+            label3.TabIndex = 6;
+            label3.Text = "Items";
+            label3.Click += label3_Click;
+            // 
+            // fileWatcher
+            // 
+            fileWatcher.EnableRaisingEvents = true;
+            fileWatcher.IncludeSubdirectories = true;
+            fileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.LastAccess;
+            fileWatcher.SynchronizingObject = this;
+            fileWatcher.Changed += fileWatcher_Changed;
+            // 
+            // buttonBrowse
+            // 
+            buttonBrowse.Location = new Point(359, 600);
+            buttonBrowse.Name = "buttonBrowse";
+            buttonBrowse.Size = new Size(173, 46);
+            buttonBrowse.TabIndex = 7;
+            buttonBrowse.Text = "Browse Folder";
+            buttonBrowse.UseVisualStyleBackColor = true;
+            buttonBrowse.Click += buttonBrowse_Click;
+            // 
+            // contextMenuStrip
+            // 
+            contextMenuStrip.ImageScalingSize = new Size(32, 32);
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { copyPathMenuItem });
+            contextMenuStrip.Name = "contextMenuStrip";
+            contextMenuStrip.Size = new Size(197, 42);
+            // 
+            // copyPathMenuItem
+            // 
+            copyPathMenuItem.Name = "copyPathMenuItem";
+            copyPathMenuItem.Size = new Size(300, 38);
+            copyPathMenuItem.Text = "Copy Path";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1146, 707);
-            Controls.Add(autoScanCheckbox);
-            Controls.Add(scanButton);
-            Controls.Add(titleLabel);
+            Controls.Add(buttonBrowse);
+            Controls.Add(label3);
+            Controls.Add(numericThreshold);
+            Controls.Add(label2);
+            Controls.Add(checkBoxAutoScan);
+            Controls.Add(buttonScanNow);
+            Controls.Add(label1);
             Controls.Add(recentListBox);
             Name = "MainForm";
             Text = "GlowDesk";
+            Load += MainForm_Load;
+            Click += MainForm_Click;
+            MouseMove += MainForm_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)numericThreshold).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fileWatcher).EndInit();
+            contextMenuStrip.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -93,8 +186,16 @@
         #endregion
 
         private ListBox recentListBox;
-        private Label titleLabel;
-        private Button scanButton;
-        private CheckBox autoScanCheckbox;
+        private Label label1;
+        private Button buttonScanNow;
+        private CheckBox checkBoxAutoScan;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private NumericUpDown numericThreshold;
+        private Label label3;
+        private FileSystemWatcher fileWatcher;
+        private Button buttonBrowse;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem copyPathMenuItem;
     }
 }
--- GlowDesk/Form1.cs
+++ GlowDesk/Form1.cs
@@ -1,25 +1,271 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace GlowDesk
 {
     public partial class MainForm : Form
     {
+        ToolTip fileToolTip = new ToolTip();
+        private string customPath = string.Empty;
+        private ContextMenuStrip contextMenu;
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeContextMenu();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void MainForm_Load(object sender, EventArgs e)
         {
+            recentListBox.DrawMode = DrawMode.OwnerDrawFixed;
 
+            fileToolTip.SetToolTip(recentListBox, "Double-click to open");
+
+            if (checkBoxAutoScan.Checked)
+            {
+                timer1.Enabled = true;
+                ScanRecentFiles();
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Setup watcher
+            fileWatcher.Path = desktopPath;
+            fileWatcher.IncludeSubdirectories = false;
+            fileWatcher.EnableRaisingEvents = checkBoxAutoScan.Checked;
+
+            // Event hooks
+            fileWatcher.Created += fileWatcher_Changed;
+            fileWatcher.Changed += fileWatcher_Changed;
+            fileWatcher.Renamed += fileWatcher_Changed;
+            fileWatcher.Deleted += fileWatcher_Changed;
+
+            // Hook up the right-click mouse event for the ListBox
+            recentListBox.MouseDown += recentListBox_MouseDown;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void buttonScanNow_Click(object sender, EventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(customPath))
+                ScanRecentFiles(customPath);
+            else
+                ScanRecentFiles(); // default desktop
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ScanRecentFiles(string folderPath = null)
         {
+            try
+            {
+                string pathToScan = folderPath ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var filesAndDirs = Directory.GetFileSystemEntries(pathToScan);
 
+                int threshold = (int)numericThreshold.Value;
+
+                var recentItems = filesAndDirs
+                    .Select(path => new FileInfo(path))
+                    .OrderByDescending(f => f.LastAccessTime)
+                    .Take(threshold)
+                    .ToList();
+
+                recentListBox.Items.Clear();
+
+                foreach (var item in recentItems)
+                {
+                    string displayText = $"{item.Name}  (Accessed: {item.LastAccessTime})";
+                    recentListBox.Items.Add(new ListBoxItemWithPath(displayText, item.FullName));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading files: " + ex.Message);
+            }
+        }
+
+        private void recentListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Optional: handle selection change if needed
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form clicked!");
+        }
+
+        private void recentListBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+
+            var item = recentListBox.Items[e.Index] as ListBoxItemWithPath;
+            string text = item?.ToString() ?? recentListBox.Items[e.Index].ToString();
+
+            // 1. Set background manually
+            Color backgroundColor = (e.State & DrawItemState.Selected) == DrawItemState.Selected
+                ? Color.LightSkyBlue
+                : recentListBox.BackColor;
+            using (SolidBrush backgroundBrush = new SolidBrush(backgroundColor))
+            {
+                e.Graphics.FillRectangle(backgroundBrush, e.Bounds);
+            }
+
+            // 2. Set text color
+            Color textColor;
+            if (e.Index == 0)
+                textColor = Color.Green;
+            else if (e.Index <= 2)
+                textColor = Color.Goldenrod;
+            else
+                textColor = Color.OrangeRed;
+
+            // 3. Draw text with a slight left and top padding
+            using (SolidBrush textBrush = new SolidBrush(textColor))
+            {
+                e.Graphics.DrawString(text, e.Font, textBrush, e.Bounds.Left + 5, e.Bounds.Top + 2);
+            }
+
+            // 4. Optional focus rectangle
+            e.DrawFocusRectangle();
+        }
+
+
+
+
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            int index = recentListBox.IndexFromPoint(e.Location);
+            if (index >= 0 && index < recentListBox.Items.Count)
+            {
+                if (recentListBox.Items[index] is ListBoxItemWithPath item)
+                {
+                    fileToolTip.SetToolTip(recentListBox, item.FullPath);
+                }
+            }
+        }
+
+        private void recentListBox_DoubleClick(object sender, EventArgs e)
+        {
+            int index = recentListBox.SelectedIndex;
+            if (index >= 0 && recentListBox.Items[index] is ListBoxItemWithPath item)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", item.FullPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Cannot open item: " + ex.Message);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ScanRecentFiles();
+        }
+
+        private void checkBoxAutoScan_CheckedChanged(object sender, EventArgs e)
+        {
+            fileWatcher.EnableRaisingEvents = checkBoxAutoScan.Checked;
+
+            if (checkBoxAutoScan.Checked)
+                timer1.Start();
+            else
+                timer1.Stop();
+        }
+
+        private void label3_Click(object sender, EventArgs e) { }
+
+        private void fileWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            this.Invoke(new Action(() => ScanRecentFiles()));
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Select a folder to scan";
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    customPath = folderDialog.SelectedPath;
+                    ScanRecentFiles(customPath);
+                }
+            }
+        }
+        private void InitializeContextMenu()
+        {
+            // Initialize ContextMenuStrip
+            contextMenu = new ContextMenuStrip();
+
+            // Create the "Copy Path" menu item
+            var copyPathMenuItem = new ToolStripMenuItem("Copy Path");
+
+            // Hook up the event handler for the click event
+            copyPathMenuItem.Click += CopyPathItem_Click;
+
+            // Add the item to the context menu
+            contextMenu.Items.Add(copyPathMenuItem);
+
+            // Set the ListBox's ContextMenuStrip property
+            recentListBox.ContextMenuStrip = contextMenu;
+        }
+
+
+        private void recentListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Ensure contextMenu is initialized
+                if (contextMenu == null)
+                {
+                    InitializeContextMenu();  // Make sure it's initialized before using it
+                }
+
+                // Get the index of the item under the mouse pointer
+                int index = recentListBox.IndexFromPoint(e.Location);
+                if (index >= 0 && index < recentListBox.Items.Count)
+                {
+                    // Set the selected item to the right-clicked item
+                    recentListBox.SelectedIndex = index;
+
+                    // Show the context menu at the mouse pointer location
+                    contextMenu.Show(recentListBox, e.Location);
+                }
+            }
+        }
+
+
+        private void CopyPathItem_Click(object sender, EventArgs e)
+        {
+            int index = recentListBox.SelectedIndex;
+            if (index >= 0 && recentListBox.Items[index] is ListBoxItemWithPath item)
+            {
+                // Copy the full path to the clipboard
+                Clipboard.SetText(item.FullPath);
+            }
+        }
+
+        public class ListBoxItemWithPath
+        {
+            public string DisplayText { get; set; }
+            public string FullPath { get; set; }
+
+            public ListBoxItemWithPath(string displayText, string fullPath)
+            {
+                DisplayText = displayText;
+                FullPath = fullPath;
+            }
+
+            public override string ToString()
+            {
+                return DisplayText;
+            }
         }
     }
+
 }
